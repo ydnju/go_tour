3ea3a9bd70f09c2a5b47bacb91fe92beef1e91cc
Yuchen Dong <yucdong@microsoft.com>
2022-03-17 14:29:55 +0800

    Streaming for GRPC

--- rpc/client/main.go
+++ rpc/client/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 
 	pb "github.com/ydnju/go_tour/rpc/proto"
@@ -14,10 +16,63 @@ func SayHello(client pb.GreeterClient) error {
 	return nil
 }
 
+func SayList(client pb.GreeterClient, r *pb.HelloRequest) error {
+	stream, _ := client.SayList(context.Background(), r)
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Resp: %v", resp)
+	}
+
+	return nil
+}
+
+func SayRecord(client pb.GreeterClient) error {
+	stream, _ := client.SayRecord(context.Background())
+	stream.Send(&pb.HelloRequest{Name: "What"})
+	stream.Send(&pb.HelloRequest{Name: "a"})
+	stream.Send(&pb.HelloRequest{Name: "Movie"})
+	reply, err := stream.CloseAndRecv()
+	if err == nil {
+		fmt.Printf("Resp: %v", reply)
+	}
+
+	return err
+}
+
+func SayRoute(client pb.GreeterClient) error {
+	stream, _ := client.SayRoute(context.Background())
+	hint, err := stream.Recv()
+	fmt.Printf("Received %v, start to transmit messages.\n", hint)
+
+	stream.Send(&pb.HelloRequest{Name: "What"})
+	stream.Send(&pb.HelloRequest{Name: "a"})
+	stream.Send(&pb.HelloRequest{Name: "Movie"})
+
+	// In our code, this must be called before
+	// stream.Recv() because the server will read
+	// EOF first then break out of the loop and return
+	// a Joined message.
+	stream.CloseSend()
+
+	reply, err := stream.Recv()
+	if err == nil {
+		fmt.Printf("Resp is: %v\n", reply)
+	}
+
+	return err
+}
+
 func main() {
 	conn, _ := grpc.Dial(":8080", grpc.WithInsecure())
 	defer conn.Close()
 
 	client := pb.NewGreeterClient(conn)
-	_ = SayHello(client)
+	_ = SayRoute(client)
 }
--- rpc/server/main.go
+++ rpc/server/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"net"
+	"strings"
 
 	pb "github.com/ydnju/go_tour/rpc/proto"
 	"google.golang.org/grpc"
@@ -13,6 +16,58 @@ type GreeterServer struct {
 
 func (s *GreeterServer) SayHello(ctx context.Context, r *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "hello, " + r.Name}, nil
+}
+
+func (s *GreeterServer) SayList(r *pb.HelloRequest, server pb.Greeter_SayListServer) error {
+	for n := 0; n < 6; n++ {
+		_ = server.Send(&pb.HelloReply{Message: "hello.list" + r.Name})
+	}
+
+	return nil
+}
+
+func (s *GreeterServer) SayRecord(server pb.Greeter_SayRecordServer) error {
+	words := []string{}
+	for {
+		resp, err := server.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		words = append(words, resp.Name)
+	}
+
+	message := strings.Join(words[:], " ")
+	server.SendAndClose(&pb.HelloReply{Message: message})
+	return nil
+}
+
+func (s *GreeterServer) SayRoute(server pb.Greeter_SayRouteServer) error {
+	n := 0
+	_ = server.Send(&pb.HelloReply{Message: "Say Route"})
+
+	pieces := []string{}
+	for {
+		resp, err := server.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		pieces = append(pieces, resp.Name)
+		n++
+	}
+
+	message := strings.Join(pieces, ",")
+	fmt.Printf("The message to response is %v\n", message)
+	server.Send(&pb.HelloReply{Message: "blabla"})
+
+	return nil
 }
 
 func main() {
